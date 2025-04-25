5fbd4a30377fe2fe114abe72692c00d0798749c6
Youssef El-Nemaky <youssef.mmdoh99@gmail.com>
2025-04-25 05:56:08 +0300

    fix: some typos and including catchAsync method

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -1,4 +1,5 @@
 const { StatusCodes } = require('http-status-codes');
+const catchAsync = require('../utils/catchAsync');
 const Job = require('../models/Job');
 
 exports.getAllJobs = catchAsync(async (req, res, next) => {
@@ -24,7 +25,7 @@ exports.createJob = catchAsync(async (req, res, next) => {
 });
 
 exports.updateJob = catchAsync(async (req, res, next) => {
-    const job = await job.findByIdAndUpdate(req.params.id, req.body, { runValidators: true, new: true });
+    const job = await Job.findByIdAndUpdate(req.params.id, req.body, { runValidators: true, new: true });
     if (!job) {
         return res
             .status(StatusCodes.NOT_FOUND)
@@ -35,12 +36,12 @@ exports.updateJob = catchAsync(async (req, res, next) => {
 });
 
 exports.deleteJob = catchAsync(async (req, res, next) => {
-    const job = await job.findByIdAndDelete();
+    const job = await Job.findByIdAndDelete(req.params.id);
     if (!job) {
         return res
             .status(StatusCodes.NOT_FOUND)
             .json({ status: 'fail', message: `No job with ID: ${req.params.id} found` });
     }
 
-    res.status(StatusCodes.NO_CONTENT);
+    res.status(StatusCodes.NO_CONTENT).json({ status: 'success' });
 });
