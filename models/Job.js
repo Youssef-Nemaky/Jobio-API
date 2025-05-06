6f00f15d9796995da0e042c646e21298cd7b0960
Youssef El-Nemaky <youssef.mmdoh99@gmail.com>
2025-05-07 00:12:25 +0300

    feat: enable timestamps for job model

--- models/Job.js
+++ models/Job.js
@@ -1,29 +1,32 @@
 const mongoose = require('mongoose');
 
-const JobSchema = mongoose.Schema({
-    company: {
-        type: String,
-        required: [true, 'Company name is required'],
-    },
-    position: {
-        type: String,
-        required: [true, 'Position is required'],
-    },
-
-    status: {
-        type: String,
-        default: 'pending',
-        enum: {
-            values: ['pending', 'interview', 'declined'],
-            message: 'Invalid value for status. You can choose from pending, interview and declined',
+const JobSchema = mongoose.Schema(
+    {
+        company: {
+            type: String,
+            required: [true, 'Company name is required'],
         },
-        required: [true, 'Status is required'],
+        position: {
+            type: String,
+            required: [true, 'Position is required'],
+        },
+
+        status: {
+            type: String,
+            default: 'pending',
+            enum: {
+                values: ['pending', 'interview', 'declined'],
+                message: 'Invalid value for status. You can choose from pending, interview and declined',
+            },
+            required: [true, 'Status is required'],
+        },
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+        },
     },
-    user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-    },
-});
+    { timestamps: true }
+);
 
 const Job = mongoose.model('Job', JobSchema);
 
